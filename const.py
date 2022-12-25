916bb89297c91b69fd649af9378c4047cbb7841f
EnzeXu <xue20@wfu.edu>
2022-12-25 06:56:03 -0500

    updates

--- const.py
+++ const.py
@@ -63,82 +63,83 @@ DIFFUSION_NAME_LIST = ["d_Am", "d_Ao", "d_Tm", "d_Tp", "d_To"]
 #         "ub": 1.0,
 #     }
 # ]
-
-DIFFUSION = [
- {
-        "id": 0,
-        "name": "d_Am",
-        "init": 1.0,
-        "lb": 0.5,
-        "ub": 2.0,
-    },
-    {
-        "id": 1,
-        "name": "d_Ao",
-        "init": 1.0,
-        "lb": 0.5,
-        "ub": 2.0,
-    },
-    {
-        "id": 2,
-        "name": "d_Tm",
-        "init": 1.0,
-        "lb": 0.5,
-        "ub": 2.0,
-    },
-    {
-        "id": 3,
-        "name": "d_Tp",
-        "init": 1.0,
-        "lb": 0.5,
-        "ub": 2.0,
-    },
-    {
-        "id": 4,
-        "name": "d_To",
-        "init": 1.0,
-        "lb": 0.5,
-        "ub": 2.0,
-    }
-]
-
-# DIFFUSION = [
-#  {
-#         "id": 0,
-#         "name": "d_Am",
-#         "init": 3.0 * 10,
-#         "lb": 1.6 * 10,
-#         "ub": 6.23 * 10,
-#     },
-#     {
-#         "id": 1,
-#         "name": "d_Ao",
-#         "init": 0.4,
-#         "lb": 0.2,
-#         "ub": 0.8,
-#     },
-#     {
-#         "id": 2,
-#         "name": "d_Tm",
-#         "init": 3.0,
-#         "lb": 1.5,
-#         "ub": 6.0,
-#     },
-#     {
-#         "id": 3,
-#         "name": "d_Tp",
-#         "init": 11.0,
-#         "lb": 5.5,
-#         "ub": 22.0,
-#     },
-#     {
-#         "id": 4,
-#         "name": "d_To",
-#         "init": 0.075,
-#         "lb": 0.05,
-#         "ub": 0.1,
-#     }
-# ]
+assert opt.diff_strategy in ["C", "D"]
+if opt.diff_strategy == "C":
+    DIFFUSION = [
+     {
+            "id": 0,
+            "name": "d_Am",
+            "init": 1.0,
+            "lb": 0.5,
+            "ub": 2.0,
+        },
+        {
+            "id": 1,
+            "name": "d_Ao",
+            "init": 1.0,
+            "lb": 0.5,
+            "ub": 2.0,
+        },
+        {
+            "id": 2,
+            "name": "d_Tm",
+            "init": 1.0,
+            "lb": 0.5,
+            "ub": 2.0,
+        },
+        {
+            "id": 3,
+            "name": "d_Tp",
+            "init": 1.0,
+            "lb": 0.5,
+            "ub": 2.0,
+        },
+        {
+            "id": 4,
+            "name": "d_To",
+            "init": 1.0,
+            "lb": 0.5,
+            "ub": 2.0,
+        }
+    ]
+else:
+    DIFFUSION = [
+     {
+            "id": 0,
+            "name": "d_Am",
+            "init": 3.0 * 10,
+            "lb": 1.6 * 10,
+            "ub": 6.23 * 10,
+        },
+        {
+            "id": 1,
+            "name": "d_Ao",
+            "init": 0.4,
+            "lb": 0.2,
+            "ub": 0.8,
+        },
+        {
+            "id": 2,
+            "name": "d_Tm",
+            "init": 3.0,
+            "lb": 1.5,
+            "ub": 6.0,
+        },
+        {
+            "id": 3,
+            "name": "d_Tp",
+            "init": 11.0,
+            "lb": 5.5,
+            "ub": 22.0,
+        },
+        {
+            "id": 4,
+            "name": "d_To",
+            "init": 0.075,
+            "lb": 0.05,
+            "ub": 0.1,
+        }
+    ]
 
 assert opt.start in ["fixed", "ranged"]
 if opt.start == "fixed":
